5115252938a9b5b990d84c7578a0ae8b8b77011b
Peace Melodi <peacemelodi1@gmail.com>
2024-05-10 01:52:12 +0100

    new

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Products.css'
 
 const Products = () => {
+
+    const [filter, setFilter] = useState("All");
+  
+   
+    const handleFilterChange = (value) => {
+        setFilter(value);
+     
+    };
+
+
+    
+
     return (
         <section>
             <div className="products">
                 <h1>PRODUCT</h1>
+                <div className="filter-btn">
+                    <button onClick={() => handleFilterChange("All")}>All</button>
+                    <button onClick={() => handleFilterChange("3D Models")} >3D Models</button>
+                    <button onClick={() => handleFilterChange("Tutorials")} > Tutorials </button>
+                </div>
                 <div className="products-box">
+                {filter === "All" || filter === "All" ? (
                     <div className='box'><div className="border">
                         <img src="https://plus.unsplash.com/premium_photo-1683887064255-1c428d0b3934?q=80&w=1486&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
@@ -14,6 +32,8 @@ const Products = () => {
                         <small>$4000</small>
                         <button>Buy</button>
                     </div>
+                         ) : null}
+                    {filter === "All" || filter === "3D Models"  ? (
                     <div className='box'><div className="border">
                         <img src="https://plus.unsplash.com/premium_photo-1683887064106-531532ecdf20?q=80&w=1486&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
@@ -21,48 +41,59 @@ const Products = () => {
                         <small>$6000</small>
                         <button>Buy</button>
                     </div>
+                    ) : null}
+                    
+                    {filter === "All" || filter === "3D Models" ? (
                     <div className='box'><div className="border">
                     <img src="https://images.unsplash.com/photo-1641391503184-a2131018701b?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <p>Barney 3D</p>
                         <small>$7000</small>
                         <button>Buy</button>
-                    </div>
+                        </div>) : null}
+                    
+                        {filter === "All" || filter === "Tutorials" ? (
                     <div className='box'><div className="border">
                         <img src="https://images.unsplash.com/photo-1637511626493-bef85b22d1d5?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <p>Ball Motion</p>
                         <small>$9000</small>
                         <button>Buy</button>
-                    </div>
+                        </div>) : null}
+                        {filter === "All" || filter === "Tutorials" ? (
                     <div className='box'><div className="border">
                         <img src="https://images.unsplash.com/photo-1635594202056-9ea3b497e5c0?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <p>4D Lady</p>
                         <small>$4900</small>
                         <button>Buy</button>
-                    </div>
+                        </div>) : null}
+                        {filter === "All" || filter === "Tutorials" ? (
+                   
                     <div className='box'><div className="border">
                         <img src="https://images.unsplash.com/photo-1624628639856-100bf817fd35?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <p>Monalisa</p>
                         <small>$4000</small>
                         <button>Buy</button>
-                    </div>
+                        </div>) : null}
+                        {filter === "All" || filter === "Tutorials" ? (
+                   
                     <div className='box'><div className="border">
                         <img src="https://images.unsplash.com/photo-1642427749670-f20e2e76ed8c?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <p>Cats and Dogs</p>
                         <small>$8900</small>
                         <button>Buy</button>
-                    </div>
+                        </div>) : null}
+                        {filter === "All" || filter === "Tutorials" ? (
                     <div className='box'><div className="border">
                         <img src="https://images.unsplash.com/photo-1633294261565-04234dc78da5?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                     </div>
                     <p>Big Man</p>
                         <small>$4000</small>
                         <button>Buy</button>
-                    </div>
+                    </div> ) : null}
                 </div>
       </div>
   </section>
